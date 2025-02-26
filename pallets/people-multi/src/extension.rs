44ede1022706a014925a9a4e7303f1338085c79c
Guillaume Thiolliere <gui.thiolliere@gmail.com>
2025-02-26 14:27:27 +0900

    init

---
+++ pallets/people-multi/src/extension.rs
@@ -0,0 +1,219 @@
+// This file is part of Substrate.
+
+// Copyright (C) 2017-2022 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! People signed extensions.
+
+use crate::*;
+use codec::{Decode, Encode};
+use core::fmt;
+use frame_support::{
+	ensure, pallet_prelude::TransactionSource, weights::Weight, CloneNoBound, DefaultNoBound,
+	EqNoBound, PartialEqNoBound,
+};
+use individuality_support::{
+	traits::Context,
+	utils::{prepare_nonce_for_account, validate_nonce_for_account, ValidNonceInfo},
+};
+use scale_info::TypeInfo;
+use sp_core::twox_64;
+use sp_runtime::{
+	traits::{DispatchInfoOf, PostDispatchInfoOf, TransactionExtension, ValidateResult},
+	transaction_validity::{InvalidTransaction, TransactionValidityError, ValidTransaction},
+};
+
+/// Information required to transform an origin into a personal alias or personal identity.
+#[derive(Encode, Decode, TypeInfo, EqNoBound, CloneNoBound, PartialEqNoBound)]
+#[scale_info(skip_type_params(T))]
+pub enum AsPersonInfo<T: Config + Send + Sync> {
+	/// The signed origin will be transformed using account to alias.
+	AsPersonalAliasWithAccount(T::Nonce),
+	/// The nonce origin will be transformed using proof.
+	AsPersonalAliasWithProof(<T::Crypto as GenerateVerifiable>::Proof, RingIndex, Context),
+	/// The nonce origin will be transformed using signature.
+	AsPersonalIdentityWithProof(<T::Crypto as GenerateVerifiable>::Signature, PersonalId),
+}
+
+/// Transaction extension to transform an origin into a personal alias or personal identity.
+#[derive(Encode, Decode, TypeInfo, EqNoBound, CloneNoBound, PartialEqNoBound, DefaultNoBound)]
+#[scale_info(skip_type_params(T))]
+pub struct AsPerson<T: Config + Send + Sync>(Option<AsPersonInfo<T>>);
+
+impl<T: Config + Send + Sync> fmt::Debug for AsPerson<T> {
+	#[cfg(feature = "std")]
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "AsPerson")
+	}
+
+	#[cfg(not(feature = "std"))]
+	fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
+		Ok(())
+	}
+}
+
+impl<T: Config + Send + Sync> AsPerson<T> {
+	pub fn new(explicit: Option<AsPersonInfo<T>>) -> Self {
+		Self(explicit)
+	}
+
+	fn weight() -> Weight {
+		// TODO: actual weight.
+		Weight::zero()
+	}
+}
+
+/// Info returned by validate to prepare in the [`AsPerson`] transactinon extension.
+pub enum Val<T: Config + Send + Sync> {
+	NotUsing,
+	UsingAliasWithAccount(T::AccountId, T::Nonce),
+	UsingAliasWithProof,
+	UsingIdentityWithProof,
+}
+
+impl<T: Config + Send + Sync> TransactionExtension<<T as frame_system::Config>::RuntimeCall>
+	for AsPerson<T>
+{
+	const IDENTIFIER: &'static str = "AsPerson";
+	type Implicit = ();
+
+	type Val = Val<T>;
+
+	// Is using tx ext
+	type Pre = bool;
+
+	fn weight(&self, _call: &<T as frame_system::Config>::RuntimeCall) -> Weight {
+		Self::weight()
+	}
+
+	fn validate(
+		&self,
+		origin: <T as frame_system::Config>::RuntimeOrigin,
+		_call: &<T as frame_system::Config>::RuntimeCall,
+		_info: &DispatchInfoOf<<T as frame_system::Config>::RuntimeCall>,
+		_len: usize,
+		_self_implicit: Self::Implicit,
+		inherited_implication: &impl Encode,
+		_source: TransactionSource,
+	) -> ValidateResult<Self::Val, <T as frame_system::Config>::RuntimeCall> {
+		match &self.0 {
+			Some(AsPersonInfo::AsPersonalAliasWithAccount(nonce)) => {
+				let Some(frame_system::Origin::<T>::Signed(who)) = origin.as_system_ref() else {
+					return Err(InvalidTransaction::BadSigner.into());
+				};
+				let who = who.clone();
+
+				let ca = AccountToAlias::<T>::get(&who).ok_or(InvalidTransaction::BadSigner)?;
+				let local_origin = Origin::PersonalAlias(ca);
+				let mut origin = origin;
+				origin.set_caller_from(local_origin);
+
+				let ValidNonceInfo { requires, provides } =
+					validate_nonce_for_account::<T>(&who, *nonce)?;
+				let validity = ValidTransaction { requires, provides, ..Default::default() };
+
+				Ok((validity, Val::UsingAliasWithAccount(who, *nonce), origin))
+			},
+			Some(AsPersonInfo::AsPersonalAliasWithProof(proof, ring, context)) => {
+				ensure!(
+					matches!(origin.as_system_ref(), Some(frame_system::RawOrigin::None)),
+					InvalidTransaction::BadSigner
+				);
+
+				let members =
+					Root::<T>::get(ring).map(|m| m.root).ok_or(InvalidTransaction::Call)?;
+
+				let msg = inherited_implication.using_encoded(sp_io::hashing::blake2_256);
+
+				let alias = T::Crypto::validate(proof, &members, &context[..], &msg[..])
+					.map_err(|_| InvalidTransaction::BadProof)?;
+
+				let ca = ContextualAlias { alias, context: *context };
+				let local_origin = Origin::PersonalAlias(ca);
+				let mut origin = origin;
+				origin.set_caller_from(local_origin);
+				// TODO: GUI: consider having some good provides and nonce.
+
+				let provides = twox_64(&(proof, ring, context, inherited_implication).encode()[..]);
+				let valid_transaction =
+					ValidTransaction::with_tag_prefix("Ppl:Alias").and_provides(provides).into();
+
+				Ok((valid_transaction, Val::UsingAliasWithProof, origin))
+			},
+			Some(AsPersonInfo::AsPersonalIdentityWithProof(signature, index)) => {
+				ensure!(
+					matches!(origin.as_system_ref(), Some(frame_system::RawOrigin::None)),
+					InvalidTransaction::BadSigner
+				);
+
+				let key = People::<T>::get(index)
+					.map(|record| record.key)
+					.ok_or(InvalidTransaction::BadSigner)?;
+
+				let msg = inherited_implication.using_encoded(sp_io::hashing::blake2_256);
+
+				if !T::Crypto::verify_signature(signature, &msg[..], &key) {
+					return Err(InvalidTransaction::BadProof.into());
+				}
+
+				let local_origin = Origin::PersonalIdentity(*index);
+				let mut origin = origin;
+				origin.set_caller_from(local_origin);
+				// TODO: GUI: consider having some good provides and nonce.
+				let provides = twox_64(&(signature, index, inherited_implication).encode()[..]);
+				let valid_transaction =
+					ValidTransaction::with_tag_prefix("Ppl:Id").and_provides(provides).into();
+
+				Ok((valid_transaction, Val::UsingIdentityWithProof, origin))
+			},
+			None => Ok((ValidTransaction::default(), Val::NotUsing, origin)),
+		}
+	}
+
+	fn prepare(
+		self,
+		val: Self::Val,
+		_origin: &<T as frame_system::Config>::RuntimeOrigin,
+		_call: &<T as frame_system::Config>::RuntimeCall,
+		_info: &DispatchInfoOf<<T as frame_system::Config>::RuntimeCall>,
+		_len: usize,
+	) -> Result<Self::Pre, TransactionValidityError> {
+		match val {
+			Val::NotUsing => Ok(false),
+			Val::UsingAliasWithAccount(who, nonce) => {
+				prepare_nonce_for_account::<T>(&who, nonce)?;
+				Ok(true)
+			},
+			Val::UsingAliasWithProof => Ok(true),
+			Val::UsingIdentityWithProof => Ok(true),
+		}
+	}
+
+	fn post_dispatch_details(
+		pre: Self::Pre,
+		_info: &DispatchInfoOf<<T as frame_system::Config>::RuntimeCall>,
+		_post_info: &PostDispatchInfoOf<<T as frame_system::Config>::RuntimeCall>,
+		_len: usize,
+		_result: &sp_runtime::DispatchResult,
+	) -> Result<Weight, TransactionValidityError> {
+		if pre {
+			// Is using tx ext
+			Ok(Weight::zero())
+		} else {
+			// Not using tx ext: some refund.
+			Ok(Self::weight())
+		}
+	}
+}
---
+++ pallets/people-multi/src/mock.rs
@@ -0,0 +1,102 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::*;
+//use frame_support::{assert_ok, ensure, ord_parameter_types, parameter_types, traits::Hooks};
+use frame_support::derive_impl;
+use sp_core::{ConstU16, ConstU32, ConstU64, H256};
+use sp_runtime::{
+	traits::{BlakeTwo256, IdentityLookup},
+	BuildStorage,
+};
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// Configure a mock runtime to test the pallet.
+frame_support::construct_runtime!(
+	pub enum Test
+	{
+		System: frame_system,
+		PeoplePallet: crate,
+	}
+);
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+	type BaseCallFilter = frame_support::traits::Everything;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type RuntimeOrigin = RuntimeOrigin;
+	type RuntimeCall = RuntimeCall;
+	type Nonce = u64;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Block = Block;
+	type RuntimeEvent = RuntimeEvent;
+	type BlockHashCount = ConstU64<250>;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ConstU16<42>;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+impl crate::Config for Test {
+	type WeightInfo = ();
+	type RuntimeEvent = RuntimeEvent;
+	type Crypto = verifiable::demo_impls::Simple;
+	type AccountContexts = ();
+	type ChunkPageSize = ConstU32<4096>;
+	type MaxRingSize = ConstU32<10>;
+}
+
+#[allow(dead_code)]
+pub fn advance_to(b: u64) {
+	while System::block_number() < b {
+		System::set_block_number(System::block_number() + 1);
+	}
+}
+
+pub struct ConfigRecord;
+
+pub fn new_config() -> ConfigRecord {
+	ConfigRecord
+}
+
+pub struct TestExt(ConfigRecord);
+#[allow(dead_code)]
+impl TestExt {
+	pub fn new() -> Self {
+		Self(new_config())
+	}
+
+	pub fn execute_with<R>(self, f: impl Fn() -> R) -> R {
+		new_test_ext().execute_with(f)
+	}
+}
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let c = frame_system::GenesisConfig::<Test>::default().build_storage().unwrap();
+	sp_io::TestExternalities::from(c)
+}
